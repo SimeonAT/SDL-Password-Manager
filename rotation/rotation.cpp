239398bf9592e90a0475d73b61c7c9f967d03a9a
Simeon <siatran@ucsc.edu>
2021-06-15 21:34:38 -0700

    Updated valgrind command and wrote basic SDL code

--- rotation/rotation.cpp
+++ rotation/rotation.cpp
@@ -1,8 +1,54 @@
-#include <stdio.h> 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <stdio.h>
 
-int main() {
-  printf("Hello World!\n");
+#define WIDTH 1280
+#define HEIGHT 720
+
+SDL_Window *mainWindow = NULL;
+SDL_Surface *mainScreen = NULL;
+
+//
+// Returns SDL error message before exiting the program.
+//
+void printError() {
+  SDL_GetError();
+  exit(1);
+}
+
+//
+// Creates the Window and accompanying Surface that we will be
+// manipulating.
+//
+void init() {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printError();
+  }
+
+  mainWindow = SDL_CreateWindow("Practice", SDL_WINDOWPOS_UNDEFINED,
+                                SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT,
+                                SDL_WINDOW_SHOWN);
+  mainScreen = SDL_GetWindowSurface(mainWindow);
+
+  return;
+}
+
+//
+// Free all dynamically allocated memory used by SDL 2.
+//
+void close() {
+  SDL_FreeSurface(mainScreen);
+  SDL_DestroyWindow(mainWindow);
+  mainScreen = NULL;
+  mainWindow = NULL;
+
+  SDL_Quit();
+  return;
+}
+
+int main(void) {
+  init();
+
+  close();
   return 0;
 }
